31e9074c641189c43069611c049a5c94cef4b253
brunoprates <brunogenovaprates@gmail.com>
2020-06-06 00:11:10 -0300

    Criado express e build

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express, { response } from 'express';
+
+const app = express();
+//2 parametros a rota users e o resposta|request
+app.get('/users', (request, response) => { 
+    console.log('Listagem de usuários');
+//retorno para o html vindo através do rsponde.json     
+    response.json([
+        'Bruno',
+        'Thais',
+        'Sofia',
+        'Familia'
+    ]);
+});
+
+app.listen(3333);
+
+
+
+
+
+
+
